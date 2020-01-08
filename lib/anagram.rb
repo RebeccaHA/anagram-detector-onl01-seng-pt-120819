95564edac6a93e311c60142f765a4278486d7e57
calm-block-7153 <rebeccahunt034@gmail.com>
2020-01-08 20:36:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,10 +6,10 @@ class Anagram
   end
   
   def match(array_of_words)
-    list = array_of_words.split(" ")
-    new = @word.split(" ")
+    list = array_of_words.split(" ").sort
+    new = @word.split(" ").sort
     list.each_with_index do |letter|
-      if letter 
+      if letter == new
     
     
 end
